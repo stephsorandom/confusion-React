947995dcd8e92ffb9de2a00fc0badd39383cb836
Stephanie Mitchell <stephsorandom@gmail.com>
2022-08-08 16:14:58 -0400

    Header and Footer Componenet added, along with styles

--- src/Components/DishdetailComponent.js
+++ src/Components/DishdetailComponent.js
@@ -55,7 +55,7 @@ const Dishdetail = (props) => {
     <div className='container'>
       <div className='row'>
         <RenderDish dish={props.dish} />
-        <RenderComments comments={props.comments} />
+        <RenderComments comments={props.dish.comments} />
       </div>
     </div>
   );
--- src/Components/MainComponent.js
+++ src/Components/MainComponent.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
+import Header from './HeaderComponent'
 import { DISHES } from '../shared/dishes';
 import Dishdetail from './DishdetailComponent';
 import Menu from './MenuComponent';
+import Footer from './FooterComponent'
 
 class Main extends Component {
   constructor(props) {
@@ -11,7 +12,7 @@ class Main extends Component {
     this.state = {
       dishes: DISHES,
       selectedDish: null,
-      comments: null
+      comments: null,
     };
   }
 
@@ -22,11 +23,7 @@ class Main extends Component {
   render() {
     return (
       <div>
-        <Navbar dark color='primary'>
-          <div className='container'>
-            <NavbarBrand href='/'>Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
+      <Header />
         <Menu
           dishes={this.state.dishes}
           onClick={(dishId) => this.onDishSelect(dishId)}
@@ -35,10 +32,11 @@ class Main extends Component {
           dish={
             this.state.dishes.filter(
               (dish) => dish.id === this.state.selectedDish
-            )[0]}
-          comments={
-            this.state.dishes.comments}
+            )[0]
+          }
+          comments={this.state.dishes.comments}
         />
+        <Footer />
       </div>
     );
   }
