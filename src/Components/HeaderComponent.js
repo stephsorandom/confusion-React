4889ea5d8d065707f1b3c2090c7dfaac517d4e84
Stephanie Mitchell <stephsorandom@gmail.com>
2022-08-11 17:46:43 -0400

    Adding Login Modal to Navbar

--- src/Components/HeaderComponent.js
+++ src/Components/HeaderComponent.js
@@ -1,23 +1,31 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
-import { Jumbotron, Navbar, NavbarBrand, NavItem, Nav, Collapse, NavbarToggler } from 'reactstrap';
+import { Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem, Jumbotron,
+  Button, Modal, ModalHeader, ModalBody } from 'reactstrap';
 
 class Header extends Component {
-
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      isNavOpen: false
-    }
-    //binding it...this.toggleNav is bound to the function below, toggleNav()...which we call onClick in <NavbarToggler/> 
+      isNavOpen: false,
+      isModalOpen: false
+    };
+    //binding it...this.toggleNav is bound to the function below, toggleNav()...which we call onClick in <NavbarToggler/>
     this.toggleNav = this.toggleNav.bind(this);
+    this.toggleModal = this.toggleModal.bind(this);
   }
 
   toggleNav() {
     this.setState({
       //taking whatever state it is currently in and giving the opposite
-      isNavOpen: !this.state.isNavOpen
-    })
+      isNavOpen: !this.state.isNavOpen,
+    });
+  }
+
+  toggleModal() {
+    this.setState({
+      isModalOpen: !this.state.isModalOpen,
+    });
   }
 
   render() {
@@ -25,39 +33,46 @@ class Header extends Component {
       <>
         <Navbar dark expand='md'>
           <div className='container'>
-          <NavbarToggler onClick={this.toggleNav} />
+            <NavbarToggler onClick={this.toggleNav} />
             <NavbarBrand className='mr-auto' href='/'>
               <img
                 src='assets/images/logo.png'
-                
                 height='30'
                 width='41'
                 alt='Ristorante Con Fusion'
               />
             </NavbarBrand>
             <Collapse isOpen={this.state.isNavOpen} navbar>
-            <Nav navbar>
-              <NavItem>
-                <NavLink className='nav-link' to='/home'>
-                  <span className='fa fa-home fa-lg'> Home</span>
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink className='nav-link' to='/aboutus'>
-                  <span className='fa fa-info fa-lg'> About Us</span>
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink className='nav-link' to='/menu'>
-                  <span className='fa fa-list fa-lg'> Menu</span>
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink className='nav-link' to='/contactus'>
-                  <span className='fa fa-address-card fa-lg'> Contact Us</span>
-                </NavLink>
-              </NavItem>
-            </Nav>
+              <Nav navbar>
+                <NavItem>
+                  <NavLink className='nav-link' to='/home'>
+                    <span className='fa fa-home fa-lg'> Home</span>
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className='nav-link' to='/aboutus'>
+                    <span className='fa fa-info fa-lg'> About Us</span>
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className='nav-link' to='/menu'>
+                    <span className='fa fa-list fa-lg'> Menu</span>
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className='nav-link' to='/contactus'>
+                    <span className='fa fa-address-card fa-lg'>
+                      {' '}
+                      Contact Us
+                    </span>
+                  </NavLink>
+                </NavItem>
+              </Nav>
+              <Nav className="ml-auto" navbar>
+                                <NavItem>
+                                    <Button outline onClick={this.toggleModal}><span className="fa fa-sign-in fa-lg"></span> Login</Button>
+                                </NavItem>
+                            </Nav>
             </Collapse>
           </div>
         </Navbar>
@@ -75,6 +90,12 @@ class Header extends Component {
             </div>
           </div>
         </Jumbotron>
+        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+                    <ModalBody>
+                    
+                    </ModalBody>
+                </Modal>
       </>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,19 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import 'bootstrap-social/bootstrap-social.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
-import 'bootstrap-social/bootstrap-social.css';
-import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
